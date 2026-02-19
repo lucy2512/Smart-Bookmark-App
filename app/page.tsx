401fb9a73e75308136763ab39638132a739521b8
lucy2512 <mail4parthib@gmail.com>
2026-02-19 14:38:24 +0530

    Fix redirect for production

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/dashboard",
+        redirectTo: `${location.origin}/dashboard`,
       },
     })
     setLoading(false)
